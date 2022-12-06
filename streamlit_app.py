66f384add0bd33e9a7683900b3982b7168fac279
Sumana <56135010+sumana28@users.noreply.github.com>
2022-12-06 10:25:24 +0000

    Update streamlit_app.py
    Correct text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My parent New Healthy Diner')
-streamlit.heading('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamit.text ('Hard-Boiled Free-Range Egg')

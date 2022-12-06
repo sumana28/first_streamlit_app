6a1cd944c696986a95572476ec374a50017343c3
Sumana <56135010+sumana28@users.noreply.github.com>
2022-12-06 11:09:42 +0000

    Update streamlit_app.py
    Add emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My parent New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text ('Hard-Boiled Free-Range Egg')
+streamlit.header( 'Breakfast Favorites')
+streamlit.text(' omega 3 & Blueberry Oatmeal')
+streamlit.text('@ Kale, Spinach & Rocket Smoothie')
+streamlit.text('4 Hard-Boiled Free-Range Egg')
+streamlit. text('0 Avocado Toast')
